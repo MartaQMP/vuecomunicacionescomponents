0622b19c8a79ba319bc267a89a0105b0a65fac0f
MartaQMP <martaquirosmp@gmail.com>
2025-10-27 14:15:14 +0100

    27/10 dia

---
+++ src/components/HijoDeporte.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>{{ nombredeporte }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HijoDeporte",
+    props: ["nombredeporte"],
+    mounted(){
+        console.log(this.nombredeporte)
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/PadreDeporte.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Padre</h1>
+        <ul>
+            <li v-for="deporte in deportes" :key="deporte">
+                <HijoDeporte :nombredeporte="deporte"/>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import HijoDeporte from "./HijoDeporte.vue";
+export default {
+    name: "PadreDeporte",
+    components: {
+        HijoDeporte,
+    },
+    data() {
+        return {
+            deportes: ["Futbol", "Baloncesto", "Formula 1"],
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
